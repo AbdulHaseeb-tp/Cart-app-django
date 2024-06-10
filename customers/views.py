d3ab52ca7ff379d149cfa316fb4c709183cbed33
AbdulHaseeb-tp <abdul786687puthiyanirathu@gmail.com>
2024-06-10 22:10:05 +0530

    second commit

--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .models import Customer
 
 # Create your views here.
+
+def show_account(request):
+    context={}
+    if request.POST and 'register' in request.POST:
+        context['register']=True
+        try:
+            username=request.POST.get('username')
+            password=request.POST.get('password')
+            email=request.POST.get('email')
+            address=request.POST.get('address')
+            phone=request.POST.get('phone')
+
+            # create user accounts
+            user=User.objects.create_user(
+                username=username,
+                password=password,
+                email=email
+            )
+
+            # create customer account
+            customer=Customer.objects.create(
+                user=user,
+                address=address,
+                phone=phone
+            )
+            success_message="User registered successfully"
+            messages.success(request,success_message)
+        except Exception as e:
+            error_message="Dupicate username or invalid inputs"
+            messages.error(request,error_message)
+
+    # login check...
+    if request.POST and 'login' in request.POST:
+        context['register']=False
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(username=username,password=password)        
+        if user:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,'You entered a invalid user credentials')        
+    return render(request, 'account.html', context)
+
+
+# logOut function
+def logoutt(request):
+    logout(request)
+    return redirect('home')
--- products/views.py
+++ products/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
-
+from . models import Product
+from django.core.paginator import Paginator
 # Create your views here.
+
+def index(request):
+
+    featured_products=Product.objects.order_by('priority')[:4]
+    latest_products=Product.objects.order_by('-id')[:4]
+    context={
+        'featured_products':featured_products,
+        'latest_products':latest_products
+        }
+    return render(request, 'index.html', context)
+
+
+def list_products(request):
+    # Pagenator page set
+    page=1
+    if request.GET:
+        page=request.GET.get('page',1) # startpage & parameter
+    # product_list=Product.objects.all()
+    product_list=Product.objects.order_by('priority')  # priority order view in html(priority,-priority_high,)
+    product_paginator=Paginator(product_list,7) # products & How many prod in 1 page
+    product_list=product_paginator.get_page(page) # rapping backend products each pages
+    context={'product':product_list}
+
+    return render(request, 'products.html',context)
+
+
+def detail_product(request,pk):
+    product=Product.objects.get(pk=pk)
+    context={'product':product}
+    return render(request, 'product_details.html', context)
